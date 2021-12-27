6bcd61d12bd053592b4f246838dd31517d50e07f
Legas-park <Legas.park@gmail.com>
2021-12-27 18:55:45 +0900

    검색기능 추가 + 검색했을때 입력값이 초기화되지 않고 어떤입력값이 검색되었는지 계속 남아있게 처리

--- src/main/java/com/legascooder/myhome/repository/BoardRepository.java
+++ src/main/java/com/legascooder/myhome/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.legascooder.myhome.repository;
 
 import com.legascooder.myhome.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,6 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
     List<Board> findByTitle(String title); // 타이틀로 검색하기위해서 인터페이스를 정의를 하면 구현은 알아서 됨
 
     List<Board> findByTitleOrContent(String title, String content); // 타이틀과 컨텐트 둘다 검색하기 위해서 검색하기 위해서 인터페이스 정의
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }

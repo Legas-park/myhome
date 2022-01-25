6cb1155e7838c0170e8ccc8279ad887ac8cd2846
Legas-park <Legas.park@gmail.com>
2022-01-25 22:27:37 +0900

    해결하지 못했던 Bean 에러 inner class 키워드 static 사용해서 해결 및 로그인, 로그아웃, 회원가입 처리와 DB에 연동 코드 작성

---
+++ src/main/java/com/legascooder/myhome/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.legascooder.myhome.repository;
+
+import com.legascooder.myhome.model.Board;
+import com.legascooder.myhome.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}

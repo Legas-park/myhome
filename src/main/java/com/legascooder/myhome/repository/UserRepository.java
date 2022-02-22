3b0a8a77520fe0883a1032575ab21f588093974c
Legas-park <Legas.park@gmail.com>
2022-02-22 19:28:09 +0900

    @EntityGraph를 사용해서 여러개의 쿼리를 생성해서 확인하지않고 한번에 확인 할 수 있도록 findAll 설정

--- src/main/java/com/legascooder/myhome/repository/UserRepository.java
+++ src/main/java/com/legascooder/myhome/repository/UserRepository.java
@@ -2,9 +2,16 @@ package com.legascooder.myhome.repository;
 
 import com.legascooder.myhome.model.Board;
 import com.legascooder.myhome.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = { "boards" })
+    List<User> findAll();
+    // 원래 model.User 에서 LAZY만 사용했을경우 정보를 호출할경우 getFiedl를 호출할때 사용되는데 @EntityGraph를 사용하면 그런 Fetch타입을 무시하고 한번에 조인이 되어서 정보를 불러 올 수 있음
+    // 여러개의 쿼리를 생성하지않고 한번에 확인
 
     User findByUsername(String username);
 
